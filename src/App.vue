3d5f62e21688707cdb15741cb37a84847a137727
Artyom Platto <platar.ant@yandex.ru>
2024-09-11 00:25:32 +0300

    some chsnges

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <template class="app">
-    <v-responsive>
-      <v-app>
-        <navBar/>
-        <v-main>
-          <router-view></router-view>
-        </v-main>
-      </v-app>
-    </v-responsive>
-  </template>
-  
-  <script setup>
-  import navBar from "./components/navBar.vue";
-    </script>
+  <v-responsive>
+    <v-app>
+      <navBar/>
+      <v-main>
+        <router-view></router-view>
+      </v-main>
+    </v-app>
+  </v-responsive>
+</template>
+
+<script setup>
+import navBar from "./components/navBar.vue";
+</script>
--- src/main.js
+++ src/main.js
@@ -1,15 +1,15 @@
-import { createApp } from "vue";
-import { createPinia } from "pinia";
+import {createApp} from "vue";
+import {createPinia} from "pinia";
 
 import App from "./App.vue";
 import {router} from './router/index.js'
-import { vuetify } from "./vuetify/vuetify.js";
+import {vuetify} from "./vuetify/vuetify.js";
 import "vuetify/dist/vuetify.min.css";
 
 const app = createApp(App);
 
 app
-  .use(createPinia())
-  .use(router)
-  .use(vuetify)
-  .mount("#app");
+    .use(createPinia())
+    .use(router)
+    .use(vuetify)
+    .mount("#app");
